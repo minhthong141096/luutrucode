4bc8ec1c98f0b6f055286ee7b4937836699c88d3
minhthong141096 <73172326+minhthong141096@users.noreply.github.com>
2020-10-21 08:36:54 +0700

    Add project files.

---
+++ nhapsongaunhien/Program.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nhapsongaunhien
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Nhap vao so a: ");
+            a = int.Parse(Console.ReadLine());
+            Random b = new Random();
+            Console.WriteLine(b.Next(0,20));
+            if (a == b.Next(0, 20))
+            {
+                Console.WriteLine("Ban da doan dung");
+            }
+            else
+                Console.WriteLine("Ban da doan sai");
+        }
+    }
+}
